a84b11ffe8ffe4cfec7e57c218dbb662736ecc2c
zigadjurovic <ziga.djurke@gmail.com>
2023-05-27 00:58:24 +0200

    FE - removed unnecessary code

--- frontend/src/components/ParcelLockers.js
+++ frontend/src/components/ParcelLockers.js
@@ -3,30 +3,28 @@ import { UserContext } from '../userContext';
 import { Navigate } from 'react-router-dom';
 import ParcelLocker from './ParcelLocker';
 import { Table, Thead, Tbody, Tr, Th, TableCaption, TableContainer, Box, Heading } from '@chakra-ui/react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import markerIconPng from 'leaflet/dist/images/marker-icon.png';
-import "leaflet/dist/images/marker-icon.png";
-import { Icon } from 'leaflet';
 
 function ParcelLockers() {
   const userContext = useContext(UserContext);
   const [parcelLockers, setParcelLockers] = useState([]);
-  const [coordinates, setCoordinates] = useState([51.505, -0.09]);
 
   useEffect(() => {
     const getParcelLockers = async () => {
-      const res = await fetch('http://localhost:3001/parcellockers', { credentials: 'include' });
-      const data = await res.json();
-      console.log(data);
-      setParcelLockers(data);
+      try {
+        const res = await fetch('http://localhost:3001/parcel-lockers', { credentials: 'include' });
+        const data = await res.json();
+        setParcelLockers(data);
+      } catch (error) {
+        console.error('Error fetching parcel lockers:', error);
+      }
     };
     getParcelLockers();
   }, []);
 
-  if (parcelLockers.length > 0) {
-    return (
-      <>
-        {!userContext.user ? <Navigate replace to="/login" /> : ''}
+  return (
+    <>
+      {!userContext.user ? <Navigate replace to="/login" /> : ''}
+      {parcelLockers.length > 0 ? (
         <TableContainer>
           <Table variant="simple" size="md">
             <TableCaption>Vsi paketniki</TableCaption>
@@ -46,21 +44,13 @@ function ParcelLockers() {
             </Tbody>
           </Table>
         </TableContainer>
+      ) : (
         <Box p={8}>
           <Heading mb={6}>Nimate paketnikov!</Heading>
         </Box>
-      </>
-    );
-  } else {
-    return (
-      <>
-        {!userContext.user ? <Navigate replace to="/login" /> : ''}
-        <Box p={12}>
-          <Heading mb={6}>Nimate paketnikov!</Heading>
-        </Box>
-      </>
-    );
-  }
+      )}
+    </>
+  );
 }
 
 export default ParcelLockers;
