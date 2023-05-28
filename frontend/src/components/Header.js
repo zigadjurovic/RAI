5f3213c45fff4a6aa40bbb953268ac572fec25dc
viktordenac <viktor.denac@gmail.com>
2023-05-28 13:24:38 +0200

    FE - basic design

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,33 +1,49 @@
 import { useContext } from "react";
 import { UserContext } from "../userContext";
 import { Link } from "react-router-dom";
+import "./styles/Header.css"; // Import CSS file for styling
 
 function Header(props) {
-    return (
-        <header>
-            <h1>{props.title}</h1>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <UserContext.Consumer>
-                        {context => (
-                            context.user ?
-                                <>
-                                    <li><Link to='/addParcelLocker'>Dodaj paketnik</Link></li>
-                                    <li><Link to='/profile'>Profile</Link></li>
-                                    <li><Link to='/logout'>Logout</Link></li>
-                                </>
-                            :
-                                <>
-                                    <li><Link to='/login'>Login</Link></li>
-                                    <li><Link to='/register'>Register</Link></li>
-                                </>
+    const { user } = useContext(UserContext); // Use useContext hook to access user context
 
+    return (
+        <header className="header">
+            <div className="header-container">
+                <Link to="/" className="logo-link">
+                    <img src="/logo.png" alt="Logo" className="logo-img" /> {/* logo */}
+                    <h1 className="logo-text">{props.title}</h1>
+                </Link>
+                <nav className="menu">
+                    <ul className="menu-items">
+                        <li className="menu-item">
+                            <Link to="/">Home</Link>
+                        </li>
+                        {user ? (
+                            <>
+                                <li className="menu-item">
+                                    <Link to="/addParcelLocker">Dodaj paketnik</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/profile">Profile</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/logout">Logout</Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="menu-item">
+                                    <Link to="/login">Login</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/register">Register</Link>
+                                </li>
+                            </>
                         )}
-                    </UserContext.Consumer>
-                </ul>
-            </nav>
-        </header >
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 }
 
