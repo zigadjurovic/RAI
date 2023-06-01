b171d7989251c96f7490ce4404d6474f01b92ee2
zigadjurovic <ziga.djurke@gmail.com>
2023-06-02 01:17:00 +0200

    Fixed - all parcels showing on page

--- frontend/src/components/ParcelLockers.js
+++ frontend/src/components/ParcelLockers.js
@@ -1,29 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { UserContext } from '../userContext';
+import { useContext } from "react";
+import { UserContext } from "../userContext";
 import { Link } from 'react-router-dom';
 import "./styles/ParcelLockers.css";
 
 const ParcelLockers = () => {
-  const { user } = useContext(UserContext);
   const [parcels, setParcels] = useState([]);
+  const { user } = useContext(UserContext); // Use useContext hook to access user context
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/parcel-lockers')
-      .then(response => {
+    const fetchParcelLockers = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/parcel-lockers/api/parcels');
         setParcels(response.data);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error(error);
-      });
+      }
+    };
+
+    fetchParcelLockers();
   }, []);
 
-  const handleDelete = async id => {
+  const handleDelete = async (id) => {
     if (window.confirm('Are you sure you want to delete this parcel locker?')) {
       try {
         await axios.delete(`http://localhost:3001/parcel-lockers/${id}`);
-        setParcels(prevParcels => prevParcels.filter(parcel => parcel._id !== id));
+        setParcels((prevParcels) => prevParcels.filter((parcel) => parcel._id !== id));
       } catch (error) {
         console.error('Error deleting parcel locker:', error);
       }
@@ -33,12 +36,12 @@ const ParcelLockers = () => {
   return (
     <div className="parcel-lockers">
       <h2>Parcel Lockers</h2>
-      {parcels.map(parcel => (
+      {parcels.map((parcel) => (
         <div key={parcel._id} className="parcel-locker-card">
           <h3>{parcel.name}</h3>
           <p>Number: {parcel.numberParcelLocker}</p>
-          {user && user.isAdmin && <Link to={`/parcel-lockers/${parcel._id}/edit`}>Edit</Link>}
-          {user && user.isAdmin && <button onClick={() => handleDelete(parcel._id)}>Delete</button>}
+          {user && user.isAdmin && <Link to={`/parcel-lockers/${parcel._id}/edit`}>Edit</Link> }
+          {user && user.isAdmin && <button onClick={() => handleDelete(parcel._id)}>Delete</button> }
         </div>
       ))}
     </div>
